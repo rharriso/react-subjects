892658a8e36c1f3c4781a7d3fdad7157c814decc
Ross Harrison <rtharrison86@gmail.com>
2016-12-09 15:29:20 -0500

    Connecto store and dispatch to it

--- subjects/MiniRedux/exercise/mini-redux/Provider.js
+++ subjects/MiniRedux/exercise/mini-redux/Provider.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 
 class Provider extends React.Component {
+
+  static PropTypes = {
+    store: PropTypes.object.isRequired
+  }
+
+  static childContextTypes = {
+    store: PropTypes.object.isRequired
+  }
+
+  getChildContext() {
+    return {
+      store: this.props.store
+    }
+  }
+
   render() {
     return <div>{this.props.children}</div>
   }
--- subjects/MiniRedux/exercise/mini-redux/connect.js
+++ subjects/MiniRedux/exercise/mini-redux/connect.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 
 const connect = (mapStateToProps) => {
   return (Component) => {
-    return Component
+    return class Connected extends React.Component {
+
+      static contextTypes = {
+        store: PropTypes.object.isRequired
+      }
+
+      state = {}
+
+      componentDidMount () {
+        const {store} = this.context;
+        this.setState({counter: store.getState()})
+
+        store.subscribe(() => {
+          this.setState({counter: store.getState()})
+        })
+      }
+
+      render () {
+        return (
+          <Component
+            dispatch={this.context.store.dispatch}
+            counter={this.state.counter}
+            />
+        )
+      }
+    }
   }
 }
 
